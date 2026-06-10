6ad96e461e56b45dca5a33cd7fc9c2349815f7b1
Daisyrie Israel Gente <daisyrieisraelgente@gmail.com>
2026-06-11 01:27:22 +0800

    corrected the database.php

--- FunRun/Backend/config/database.php
+++ FunRun/Backend/config/database.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
-$dbname = "fun_run_db";
-$username = "root";
-$password = "";
+$host = "your-project-ref.supabase.co";
+$dbname = "postgres";
+$username = "postgres";
+$password = "your-supabase-db-password";
+$port = "5432";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     header("Content-Type: application/json");
